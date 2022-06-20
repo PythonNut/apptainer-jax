e52ac789e0ba0a2910b867f8aff0ae0a7e1a8714
PythonNut <PythonNut@users.noreply.github.com>
2022-06-19 18:53:39 -0700

    Add nvidia-smi

--- cuda-jax.sh
+++ cuda-jax.sh
@@ -1,5 +1,10 @@
 set -o errexit
 
+export DEBIAN_FRONTEND=noninteractive
+sed -i -e's/ main/ main contrib non-free/g' /etc/apt/sources.list
+apt update
+apt install -y nvidia-smi
+
 eval "$(micromamba shell hook --shell=bash)"
 micromamba create -n cuda-jax
 micromamba install -q -y -n cuda-jax -f env.yml
