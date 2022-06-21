9cb37a9b5c528aa46b6b61bfcf47ed3362d77c31
PythonNut <PythonNut@users.noreply.github.com>
2022-06-21 16:21:47 -0700

    Make wrapper runnable on non-slurm machines

--- cuda-jax-wrapper.sh
+++ cuda-jax-wrapper.sh
@@ -1,6 +1,27 @@
 #!/bin/env bash
 
-module load apptainer
-module load cuda/11.6.2
-scriptdir="$(dirname -- $(readlink $BASH_SOURCE))";
-apptainer run -B /gscratch -B /mmfs1 -B /sw -B /run/munge -B /var/spool/slurmd/conf-cache --nv $scriptdir/cuda-jax.sif "$@"
+if command -v module &> /dev/null; then
+    module load apptainer
+    module load cuda/11.6.2
+fi
+
+scriptdir="$(dirname -- $(readlink -f $BASH_SOURCE))";
+
+typeset -a mounts=(
+    /gscratch
+    /mmfs1
+    /sw
+    /run/munge
+    /var/spool/slurmd/conf-cache
+)
+
+mount_flags=()
+for m in ${mounts[@]}; do
+    echo $m
+    if [[ -d "$m" ]]; then
+        mount_flags+=(-B)
+        mount_flags+=("$m")
+    fi
+done
+
+apptainer run ${mount_flags[@]} --nv $scriptdir/cuda-jax.sif "$@"
