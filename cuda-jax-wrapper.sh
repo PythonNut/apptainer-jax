fd3f18c8c95dfb49ac1106472f58ceebba20f311
PythonNut <PythonNut@users.noreply.github.com>
2022-06-21 15:39:42 -0700

    Enable controlling slurm from inside Apptainer

--- cuda-jax-wrapper.sh
+++ cuda-jax-wrapper.sh
@@ -3,4 +3,4 @@
 module load apptainer
 module load cuda/11.6.2
 scriptdir="$(dirname -- $(readlink $BASH_SOURCE))";
-apptainer run -B /gscratch -B /mmfs1 -B /sw --nv $scriptdir/cuda-jax.sif "$@"
+apptainer run -B /gscratch -B /mmfs1 -B /sw -B /run/munge -B /var/spool/slurmd/conf-cache --nv $scriptdir/cuda-jax.sif "$@"
--- cuda-jax.sh
+++ cuda-jax.sh
@@ -3,11 +3,17 @@ set -o errexit
 export DEBIAN_FRONTEND=noninteractive
 sed -i -e's/ main/ main contrib non-free/g' /etc/apt/sources.list
 apt update
-apt install -y nvidia-smi
+apt install -y nvidia-smi slurm-wlm libslurm-dev libslurm-perl libslurmdb-perl slurm-wlm-basic-plugins-dev git gcc gcc-multilib strace gdb
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/slurm-wlm/:$LIBRARY_PATH
+
+mkdir -p /run/slurm /etc/slurm
+ln -s /var/spool/slurmd/conf-cache/ /run/slurm/conf
+ln -s /var/spool/slurmd/conf-cache/ /etc/slurm
 
 eval "$(micromamba shell hook --shell=bash)"
 micromamba create -n cuda-jax
 micromamba install -q -y -n cuda-jax -f env.yml
 micromamba clean --all --yes
 micromamba activate cuda-jax
+
 poetry install
