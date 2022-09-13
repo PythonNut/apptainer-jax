cead5a736512a14363aaea45b4dd2eb071aceea2
PythonNut <PythonNut@users.noreply.github.com>
2022-09-12 17:34:17 -0700

    Add ffcv, nltk, clip

--- cuda-jax.sh
+++ cuda-jax.sh
@@ -2,9 +2,10 @@ set -o errexit
 
 export DEBIAN_FRONTEND=noninteractive
 sed -i -e's/ main/ main contrib non-free/g' /etc/apt/sources.list
-apt update
-apt install -y nvidia-smi slurm-wlm libslurm-dev libslurm-perl libslurmdb-perl slurm-wlm-basic-plugins-dev git gcc gcc-multilib strace gdb
-apt clean
+apt-get update
+apt-get install -y nvidia-smi slurm-wlm libslurm-dev libslurm-perl libslurmdb-perl slurm-wlm-basic-plugins-dev \
+    git gcc gcc-multilib strace gdb libopencv-dev libjpeg62-turbo-dev libturbojpeg0-dev
+apt-get clean
 export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/slurm-wlm/:$LIBRARY_PATH
 
 mkdir -p /run/slurm /etc/slurm
